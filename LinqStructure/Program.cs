7c4c7a363ece78523e551696fd0835fea00e3fdb
kostyaHrytsyuk <streetman889@gmail.com>
2018-07-02 21:08:28 +0300

    Add project files.

---
+++ LinqStructure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
