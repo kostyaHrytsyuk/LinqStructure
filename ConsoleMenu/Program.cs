521769b4d250872937fa94f2077be7511b5a6602
kostyaHrytsyuk <streetman889@gmail.com>
2018-07-07 19:18:52 +0300

    Divided LinqStructure project to the Console Application and DLL

---
+++ ConsoleMenu/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using LinqStructure;
+
+namespace ConsoleMenu
+{
+    class Program
+    {
+        private static LinqService _service = LinqService.Service;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!\nLet's work with users data!");
+            Thread.Sleep(1200);
+            Console.Clear();
+
+            Menu.MenuMap();
+
+        }
+    }
+}
